eb29d8ff2c783f16d7beab46dfc34a62c6bf6e4f
Unknown <support@dler.io>
2025-03-11 16:27:31 +0800

    bochk.com

--- Surge/Surge 4/Script/chatgpt_check.js
+++ Surge/Surge 4/Script/chatgpt_check.js
@@ -10,15 +10,15 @@ iconerr-color：不能使用时图标颜色
 */
 
 let url = "http://chat.openai.com/cdn-cgi/trace";
-let tf=["T1", "XX", "AL", "DZ", "AD", "AO", "AG", "AR", "AM", "AU", "AT", "AZ", "BS", "BD", "BB", "BE", "BZ", "BJ", "BT", "BA", "BW", "BR", "BG", "BF", "CV", "CA", "CL", "CO", "KM", "CR", "HR", "CY", "DK", "DJ", "DM", "DO", "EC", "SV", "EE", "FJ", "FI", "FR", "GA", "GM", "GE", "DE", "GH", "GR", "GD", "GT", "GN", "GW", "GY", "HT", "HN", "HU", "IS", "IN", "ID", "IQ", "IE", "IL", "IT", "JM", "JP", "JO", "KZ", "KE", "KI", "KW", "KG", "LV", "LB", "LS", "LR", "LI", "LT", "LU", "MG", "MW", "MY", "MV", "ML", "MT", "MH", "MR", "MU", "MX", "MC", "MN", "ME", "MA", "MZ", "MM", "NA", "NR", "NP", "NL", "NZ", "NI", "NE", "NG", "MK", "NO", "OM", "PK", "PW", "PA", "PG", "PE", "PH", "PL", "PT", "QA", "RO", "RW", "KN", "LC", "VC", "WS", "SM", "ST", "SN", "RS", "SC", "SL", "SG", "SK", "SI", "SB", "ZA", "ES", "LK", "SR", "SE", "CH", "TH", "TG", "TO", "TT", "TN", "TR", "TV", "UG", "AE", "US", "UY", "VU", "ZM", "BO", "BN", "CG", "CZ", "VA", "FM", "MD", "PS", "KR", "TW", "TZ", "TL", "GB"];
-let tff=["plus", "on"];
+let tf=["T1","XX","AL","DZ","AD","AO","AG","AR","AM","AU","AT","AZ","BS","BD","BB","BE","BZ","BJ","BT","BA","BW","BR","BG","BF","CV","CA","CL","CO","KM","CR","HR","CY","DK","DJ","DM","DO","EC","SV","EE","FJ","FI","FR","GA","GM","GE","DE","GH","GR","GD","GT","GN","GW","GY","HT","HN","HU","IS","IN","ID","IQ","IE","IL","IT","JM","JP","JO","KZ","KE","KI","KW","KG","LV","LB","LS","LR","LI","LT","LU","MG","MW","MY","MV","ML","MT","MH","MR","MU","MX","MC","MN","ME","MA","MZ","MM","NA","NR","NP","NL","NZ","NI","NE","NG","MK","NO","OM","PK","PW","PA","PG","PE","PH","PL","PT","QA","RO","RW","KN","LC","VC","WS","SM","ST","SN","RS","SC","SL","SG","SK","SI","SB","ZA","ES","LK","SR","SE","CH","TH","TG","TO","TT","TN","TR","TV","UG","AE","US","UY","VU","ZM","BO","BN","CG","CZ","VA","FM","MD","PS","KR","TW","TZ","TL","GB"];
+let tff=["plus","on"];
 
 // 处理 argument 参数
-let titlediy, icon, iconerr, iconColor, iconerrColor;
+let titlediy,icon,iconerr,iconColor,iconerrColor;
 if (typeof $argument !== 'undefined') {
   const args = $argument.split('&');
   for (let i = 0; i < args.length; i++) {
-  const [key, value] = args[i].split('=');
+  const [key,value] = args[i].split('=');
   if (key === 'title') {
     titlediy = value;
   } else if (key === 'icon') {
@@ -34,7 +34,7 @@ if (typeof $argument !== 'undefined') {
 }
 
 // 发送 HTTP 请求获取所在地信息
-$httpClient.get(url, function(error, response, data){
+$httpClient.get(url,function(error,response,data){
   if (error) {
   console.error(error);
   $done();
@@ -42,18 +42,18 @@ $httpClient.get(url, function(error, response, data){
   }
 
   let lines = data.split("\n");
-  let cf = lines.reduce((acc, line) => {
-  let [key, value] = line.split("=");
+  let cf = lines.reduce((acc,line) => {
+  let [key,value] = line.split("=");
   acc[key] = value;
   return acc;
-  }, {});
+  },{});
   let ip = cf.ip;
   let warp = cf.warp;
   let loc = getCountryFlagEmoji(cf.loc) + ' ' + cf.loc;
 
   // 判断 ChatGPT 是否支持该国家/地区
   let l = tf.indexOf(cf.loc);
-  let gpt, iconUsed;
+  let gpt,iconUsed;
   if (l !== -1) {
   gpt = "支持";
   iconUsed = icon ? icon : undefined;
@@ -77,9 +77,9 @@ $httpClient.get(url, function(error, response, data){
 
   // 组装通知数据
   let body = {
-    title: titlediy ? titlediy : 'ChatGPT', 
-    content: `${gpt} | 地区: ${loc}`, 
-    icon: iconUsed ? iconUsed : undefined, 
+    title: titlediy ? titlediy : 'ChatGPT',
+    content: `${gpt} | 地区: ${loc}`,
+    icon: iconUsed ? iconUsed : undefined,
     'icon-color': iconCol ? iconCol : undefined
   };
 
